373c00362bd15f7455ae8831ef89ba449e9f2871
Nikkon97 <you@example.com>
2023-06-25 15:18:58 +0300

    init commit

---
+++ app/catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    template = 'catalog/home.html'
+    return render(request, template, {})
+
+
+def contacts(request):
+    template = 'catalog/contacts.html'
+    context = {}
+    if request.method == 'POST':
+        context.update(request.POST.dict())
+
+    return render(request, template, context)
